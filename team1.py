480ef4c5b9f0b367edc1951ce8904deb34fa142f
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-12-21 11:06:48 +0530

    add pir chart 2

--- team1.py
+++ team1.py
@@ -4,7 +4,7 @@ import streamlit as st
 # add a title to your section
 title = 'Team 1'
 # add a published csv link from google sheets
-google_sheet_link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv'
+google_sheet_link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQvZjeq6jPoMOfeJIS11esCTGpE92W7QvC7XwLyGlZi23TYjgBL1aImsn_dW8iUxKSw_qhEDA_be59y/pub?gid=1847193799&single=true&output=csv'
 
 #############################################################
 google_sheet_data = fetch_google_sheet_data(google_sheet_link)
@@ -12,5 +12,7 @@ google_sheet_data = fetch_google_sheet_data(google_sheet_link)
 def page():   
     st.subheader(title)
     st.write(google_sheet_data)
+
+    # google_sheet_data = df[google_sheet_data]
 
     generate_pie_chart(google_sheet_data, 'Home MC')
