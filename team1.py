f73a7e656e79c84f08b8f7bbbf78db4b78340599
Dinuka Avinash <115539642+dinuka-avinash@users.noreply.github.com>
2024-12-21 09:17:39 +0530

    Update team1.py -  add link of csv

--- team1.py
+++ team1.py
@@ -4,7 +4,7 @@ import streamlit as st
 # add a title to your section
 title = 'Team 1'
 # add a published csv link from google sheets
-google_sheet_link = ''
+google_sheet_link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cEIAKQ26fGRkvr8hnBqkmuWfzverObjQdgcC3mdbdmIx7P0QOauwzXcC0Uz_aWDfDnaKAhhp3BST/pub?output=csv'
 
 #############################################################
 google_sheet_data = fetch_google_sheet_data(google_sheet_link)
