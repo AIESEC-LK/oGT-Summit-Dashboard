5a768d09693da79ae8e9445774cedad5d0ae3f73
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-12-21 12:55:34 +0530

    link updated

--- team2.py
+++ team2.py
@@ -7,7 +7,6 @@ title = 'Team 2'
 
 # add a published csv link from google sheets
 google_sheet_link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQvZjeq6jPoMOfeJIS11esCTGpE92W7QvC7XwLyGlZi23TYjgBL1aImsn_dW8iUxKSw_qhEDA_be59y/pub?gid=1847193799&single=true&output=csv'
-
 #############################################################
 google_sheet_data = fetch_google_sheet_data(google_sheet_link)
 
